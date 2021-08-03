6e1f76acc958bceff87bc396d75ec425b0929111
Eugene Sirotinski <esirotinski@gmail.com>
2021-08-03 14:23:51 +0300

    Add wishlists app and run migrations.

--- src/webshop/webshop/settings.py
+++ src/webshop/webshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products.apps.ProductsConfig',
+    'wishlists.apps.WishlistsConfig',
 ]
 
 MIDDLEWARE = [
---
+++ src/webshop/wishlists/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from products.models import Product
+
+
+User = get_user_model()
+
+class Wishlist(models.Model):
+    name = models.CharField(max_length=64, blank=False, null=False)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.name
